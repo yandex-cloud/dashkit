7a6c32364c133d4d20da3324ba733a1ab2a472b7
Anastasia Beria <estasieberia@yandex-team.ru>
2023-07-14 16:55:38 +0400

    feat: add imports of actions toolbar

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export * from './DashKit';
+export {ActionsToolbar} from './ActionsToolbar/ActionsToolbar';
+export type {ActionsToolbarProps} from './ActionsToolbar/ActionsToolbar';
 export type {OverlayControlItem} from './OverlayControls/OverlayControls';
